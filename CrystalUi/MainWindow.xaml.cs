16fadb3b79e6b7414a394c2d5443f2259866fca4
chaosannals <chaosannals@outlook.com>
2021-10-19 16:29:45 +0800

    水晶报表

---
+++ CrystalUi/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+using CrystalDecisions.CrystalReports.Engine;
+
+namespace CrystalUi
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ReportDocument report;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void onClickOpen(object sender, RoutedEventArgs e)
+        {
+            if (report == null)
+            {
+                report = new ReportDocument();
+                
+                // report.Load("");
+                DataSet ds = new DataSet();
+                DataTable dt = new DataTable();
+                dt.Rows.Add("第一行");
+                ds.Tables.Add(dt);
+                report.SetDataSource(ds);
+            }
+            
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            if (report != null)
+            {
+                report.Dispose();
+                report = null;
+            }
+        }
+    }
+}
