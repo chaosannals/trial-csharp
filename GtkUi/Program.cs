02f36f25a5a335fe1a1d4791862068c94bb9b8da
chaosannals <chaosannals@outlook.com>
2021-10-19 14:29:05 +0800

    gtk

---
+++ GtkUi/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using Gtk;
+
+namespace GtkUi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Application.Init();
+            var window = new Window("Gtk Window");
+            window.WindowPosition = WindowPosition.Center;
+            window.SetDefaultSize(800, 600);
+            window.Resizable = false;
+            window.DeleteEvent += (s, e) =>
+            {
+                Application.Quit();
+            };
+            window.ShowAll();
+            var builder = new Builder();
+            builder.AddFromString(Encoding.UTF8.GetString(Properties.Resources.one));
+            var w = builder.GetObject("window") as Window;
+            w.ShowAll();
+            Application.Run();
+        }
+    }
+}
---
+++ GtkUi/Readme.md
@@ -0,0 +1,3 @@
+# GTK UI
+
+在 [Gnome 下载](https://download.gnome.org/) 可以找到 [Glade](https://download.gnome.org/binaries/win32/glade) 的二进制，但是版本比较老是 2012 年的。
