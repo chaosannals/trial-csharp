428914413c8e85b1f361ed61165a3e66abe98c9c
chaosannals <chaosannals@outlook.com>
2021-11-27 14:46:12 +0800

    TimeDate

---
+++ TimeDate/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace TimeDate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i <= 100; i++)
+            {
+                Console.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
