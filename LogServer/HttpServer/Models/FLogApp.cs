d6f92677d3e5f993ee1be75bc22eeaace1da24d9
chaosannals <chaosannals@outlook.com>
2022-06-21 10:36:44 +0800

    save

---
+++ LogServer/HttpServer/Models/FLogApp.cs
@@ -0,0 +1,24 @@
+﻿using FreeSql.DataAnnotations;
+
+namespace HttpServer.Models;
+
+[Table(Name = "log_app")]
+[Index("ACCOUNT_UNIQUE", "account", IsUnique = true)]
+[Index("CREATE_AT_INDEX", "create_at")]
+public class FLogApp
+{
+    [Column(Name = "id", IsPrimary = true, IsIdentity = true)]
+    public long Id { get; set; }
+
+    [Column(Name ="account", IsNullable = false)]
+    public string Account { get; set; } = null!;
+
+    [Column(Name ="secret", IsNullable = false)]
+    public string Secret { get; set; } = null!;
+
+    [Column(Name = "create_at", IsNullable = false)]
+    public DateTime CreateAt { get; set; }
+
+    [Column(Name = "remark", IsNullable = true)]
+    public string? Remark { get; set; }
+}
--- LogServer/HttpServer/Models/FLogRecord.cs
+++ LogServer/HttpServer/Models/FLogRecord.cs
@@ -4,15 +4,19 @@ using FreeSql.DatabaseModel;
 namespace HttpServer.Models;
 
 [Table(Name ="log_record")]
+[Index("APP_INDEX", "app_id, create_at")]
+[Index("CREATE_AT_INDEX", "create_at")]
 public class FLogRecord
 {
-    [Column(IsPrimary =true, IsIdentity = false)]
+    [Column(Name="id", IsPrimary =true, IsIdentity = false)]
     public long Id { get; set; }
 
+    [Column(Name ="app_id", IsNullable =false)]
     public long AppId { get; set; }
 
-    [Column(DbType = "TEXT")]
+    [Column(Name = "content", DbType = "TEXT", IsNullable =false)]
     public string Content { get; set; } = null!;
 
+    [Column(Name ="create_at", IsNullable =false)]
     public DateTime CreateAt { get; set; }
 }
