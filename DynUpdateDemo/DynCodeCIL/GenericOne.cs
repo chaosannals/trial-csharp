05afa745846e729dede2572498448153a65003fd
chenshenchao <chenshenchao@outlook.com>
2022-06-24 01:09:07 +0800

    save

---
+++ DynUpdateDemo/DynCodeCIL/GenericOne.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynCodeCIL;
+
+public class GenericOne<T1, T2>
+{
+    public T1? MyT1 { get; set; } = default(T1);
+    private T2? t2 = default(T2);
+}
