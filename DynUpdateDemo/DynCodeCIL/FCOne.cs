c476e0b6b5076d06c8e5a066fbf5e2b67eafb761
chenshenchao <chenshenchao@outlook.com>
2022-06-26 23:26:40 +0800

    save

---
+++ DynUpdateDemo/DynCodeCIL/FCOne.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynCodeCIL;
+
+public class FCOne
+{
+    private int c;
+    public FCOne(int v)
+    {
+        c = v;
+    }
+
+    public int Func(int a, int b)
+    {
+        return a + b + c;
+    }
+}
