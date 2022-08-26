b492674016de14fdc6c0908ba47e9ecbf4ee8a30
chaosannals <chaosannals@outlook.com>
2022-08-26 10:37:09 +0800

    cpu temperature

---
+++ HardDemo/CpuInfo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using OpenHardwareMonitor.Hardware;
+
+
+namespace CpuInfo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            UpdateVisitor updateVisitor = new UpdateVisitor();
+            Computer computer = new Computer();
+            computer.Open();
+            computer.CPUEnabled = true;
+            computer.Accept(updateVisitor);
+            for (int i = 0; i < computer.Hardware.Length; i++)
+            {
+                if (computer.Hardware[i].HardwareType == HardwareType.CPU)
+                {
+                    for (int j = 0; j < computer.Hardware[i].Sensors.Length; j++)
+                    {
+                        if (computer.Hardware[i].Sensors[j].SensorType == SensorType.Temperature)
+                        {
+                            Console.WriteLine(computer.Hardware[i].Sensors[j].Name + ":" + computer.Hardware[i].Sensors[j].Value.ToString() + "\r");
+                        }
+                    }
+                }
+            }
+            computer.Close();
+            Console.ReadKey();
+        }
+    }
+}
