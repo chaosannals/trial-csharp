2da52398abdac3b029003c17db134d009400b2d8
chaosannals <chaosannals@outlook.com>
2022-10-21 16:36:18 +0800

    save

---
+++ LogServer/GrpcClient/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using GrpcClient.Services;
+using GrpcClient;
+using Grpc.Core;
+using Grpc.Net.Client;
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureHostConfiguration(cd =>
+    {
+        cd.SetBasePath(Directory.GetCurrentDirectory());
+        cd.AddEnvironmentVariables(prefix: "GRPC_CLIENT_");
+    })
+    .ConfigureLogging((hc, cl) =>
+    {
+        cl.AddFile(hc.Configuration.GetSection("LoggingFile"));
+        cl.AddConsole();
+    })
+    .ConfigureServices((hc, services) =>
+    {
+        services.AddSingleton<ChannelBase>(op =>
+        {
+            var conf = op.GetRequiredService<IConfiguration>();
+            var host = conf.GetValue<string>("GrpcServer:Host");
+            var port = conf.GetValue<int>("GrpcServer:Port");
+            return GrpcChannel.ForAddress($"https://{host}:{port}");
+        });
+        services.AddSingleton<Greeter.GreeterClient>();
+        services.AddHostedService<GreeterClientService>();
+    })
+    .UseConsoleLifetime()
+    .Build();
+
+await host.RunAsync();
---
+++ LogServer/GrpcClient/Services/GreeterClientService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace GrpcClient.Services;
+
+public class GreeterClientService : BackgroundService
+{
+    private Greeter.GreeterClient greeterClient;
+    private ILogger<GreeterClientService> logger;
+
+    public GreeterClientService(Greeter.GreeterClient greeterClient, ILogger<GreeterClientService> logger)
+    {
+        this.greeterClient=greeterClient;
+        this.logger=logger;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            var reply = await greeterClient.SayHelloAsync(
+                new HelloRequest { Name = "TestClient Greeter "}
+            );
+            logger.LogInformation("Greeting Client: {0}", reply.Message);
+            await Task.Delay(1000);
+        }
+    }
+}
