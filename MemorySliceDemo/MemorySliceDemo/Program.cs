63e247a409722801709248cf0328eee94453531e
chaosannals <chaosannals@outlook.com>
2022-11-15 17:58:14 +0800

    save

---
+++ MemorySliceDemo/MemorySliceDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+using System.Runtime.InteropServices;
+
+unsafe
+{
+    var p = stackalloc ExtraStruct[1];
+    Span<ExtraStruct> pa = new Span<ExtraStruct>(p, 1);
+    IntPtr pb = new IntPtr(p);
+    var sb = Marshal.PtrToStructure<ExtraStruct>(pb);
+
+    var ai = new int[123];
+    fixed(int* ptr = ai)
+    {
+        
+    }
+}
+
+
+string s = "dfgdfgdgd";
+var ss = s[1..3];
+var sss = s.AsSpan().Slice(1);
+
+var a = new int[] { 1, 2, 3, 4 };
+var aa = a[0..1];
+
+struct ExtraStruct
+{
+    public int aint;
+    public char achar;
+    public float afloat;
+}
