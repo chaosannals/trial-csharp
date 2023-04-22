08e09a42a309dc1abad199c575b97932bd648ef5
chaosannals <chaosannals@outlook.com>
2023-04-22 18:02:58 +0800

    SAVE

---
+++ TxIMDemo/TxIMDemo/Controllers/TxIMController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.AccessControl;
+using TxIMDemo.Services;
+
+namespace TxIMDemo.Controllers;
+
+[ApiController]
+[Route("txim")]
+public class TxIMController : ControllerBase
+{
+    private readonly TxIMService txIMService;
+
+    public TxIMController(TxIMService txim)
+    {
+        txIMService = txim;
+    }
+
+    [HttpGet(Name = "signature")]
+    public async Task<object> Signature(string username)
+    {
+        await Task.Yield();
+
+        return new
+        {
+            Code = 0,
+            Signature = txIMService.GenerateSignature(username),
+        };
+    }
+}
---
+++ TxIMDemo/TxIMDemo/Services/TxIMService.cs
@@ -0,0 +1,25 @@
+﻿
+
+using tencentyun;
+
+namespace TxIMDemo.Services;
+
+public class TxIMService
+{
+    private TLSSigAPIv2 signer;
+    private ILogger<TxIMService> logger;
+
+    public TxIMService(IConfiguration configuration, ILogger<TxIMService> logger)
+    {
+        this.logger = logger;
+        var appId = configuration.GetValue<int>("TxIM:AppId", 0);
+        var appKey = configuration.GetValue("TxIM:AppKey", "");
+        signer = new TLSSigAPIv2(appId, appKey);
+        logger.LogInformation("appid: {} ; appKey: {}", appId, appKey);
+    }
+
+    public string GenerateSignature(string username)
+    {
+        return signer.GenSig(username);
+    }
+}
