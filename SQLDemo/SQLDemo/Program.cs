ce782d6ccb7c3076d078f747cf61501b78247414
chaosannals <chaosannals@outlook.com>
2024-05-17 15:24:03 +0800

    save

---
+++ SQLDemo/SQLDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.SqlClient;
+
+var sqlBuilder = new SqlConnectionStringBuilder
+{
+    DataSource = "localhost",
+    InitialCatalog = "mainDB",
+    Password = "Y;9r.5JQ6cwy@)V_",
+    UserID = "sqluser"
+};
+
+
+// 通过这个函数，可以查看 SQL SERVER 构建的 链接字符串。
+Console.WriteLine(sqlBuilder.ToString());
+Console.ReadKey();
